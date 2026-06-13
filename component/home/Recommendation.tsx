996a80a649b0c68b98810e55916ac8184f169e48
EverAzim <113935317+Azimkhan01@users.noreply.github.com>
2026-06-13 15:10:26 +0530

    Update Recommendation.tsx

--- component/home/Recommendation.tsx
+++ component/home/Recommendation.tsx
@@ -4,17 +4,13 @@ import CurveDivider from "../common/Wave";
 function Recommendation() {
   return (
     <div>
-      <CurveDivider
-        direction="up"
-        color="#fff"
-      />
+      <CurveDivider color="#fff" />
 
       <section className="h-screen bg-white">
         Content
       </section>
 
       <CurveDivider
-        direction="down"
         color="#F3F4F6"
         className="bg-white"
       />
