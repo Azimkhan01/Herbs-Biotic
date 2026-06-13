49b6be24743bda790976cd39929d42123763d91c
EverAzim <113935317+Azimkhan01@users.noreply.github.com>
2026-06-13 14:55:44 +0530

    Update Wave.tsx

--- component/common/Wave.tsx
+++ component/common/Wave.tsx
@@ -7,79 +7,58 @@ import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
-interface WaveProps {
-  direction?: "up" | "down";
+interface CurveProps {
   color?: string;
   className?: string;
 }
 
-export default function Wave({
-  direction = "down",
+export default function CurveDivider({
   color = "#fff",
   className = "",
-}: WaveProps) {
+}: CurveProps) {
   const pathRef = useRef<SVGPathElement>(null);
 
   useGSAP(() => {
-    const startLeft = direction === "down" ? 180 : 20;
-    const startRight = direction === "down" ? 20 : 180;
+    const curve = { y: -150 };
 
-    const endLeft = direction === "down" ? 20 : 180;
-    const endRight = direction === "down" ? 180 : 20;
-
-    const updatePath = (leftY: number, rightY: number) => {
+    const updatePath = () => {
       pathRef.current?.setAttribute(
         "d",
-        `M0,100
-         C360,${leftY}
-         1080,${rightY}
-         1440,100
-         L1440,200
-         L0,200
-         Z`
+        `
+        M0,0
+        Q720,${curve.y} 1440,0
+        L1440,200
+        L0,200
+        Z
+      `
       );
     };
 
-    const trigger = ScrollTrigger.create({
-      trigger: document.documentElement,
-      start: "top top",
-      end: "bottom bottom",
-      scrub: true,
-      onUpdate: ({ progress }) => {
-        const leftY = gsap.utils.interpolate(
-          startLeft,
-          endLeft,
-          progress
-        );
+    updatePath();
 
-        const rightY = gsap.utils.interpolate(
-          startRight,
-          endRight,
-          progress
-        );
-
-        updatePath(leftY, rightY);
+    gsap.to(curve, {
+      y: 150,
+      ease: "none",
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true,
       },
+      onUpdate: updatePath,
     });
-
-    return () => trigger.kill();
-  }, [direction]);
+  }, []);
 
   return (
     <svg
       viewBox="0 0 1440 200"
       preserveAspectRatio="none"
-      className={`w-full h-[200px]  ${className}`}
+      className={`w-full h-[200px] ${className}`}
     >
       <path
-      
         ref={pathRef}
         fill={color}
-        d={
-          direction === "down"
-            ? "M0,100 C360,180 1080,20 1440,100 L1440,200 L0,200 Z"
-            : "M0,100 C360,20 1080,180 1440,100 L1440,200 L0,200 Z"
-        }
+        d="M0,0 Q720,-150 1440,0 L1440,200 L0,200 Z"
       />
     </svg>
   );
