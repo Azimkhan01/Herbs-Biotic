60ee6e2377b036b07bc17f667653d69ac87ea35b
Azimkhan01 <azimkarimrahim@gmail.com>
2026-06-28 19:25:42 +0530

    some important update

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import Page2 from "@/component/home/Page2";
 import QnA from "@/component/home/QnA";
 import QNA2 from "@/component/home/QNA2";
 import Recommendation from "@/component/home/Recommendation";
-import Search from "@/component/home/Search";
 
 export default function Home() {
   return (
     <>
     <Hero/>
-    <Search/>
     <Recommendation/>    
     <QnA/>
     <Page2/>
